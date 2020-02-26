4c141f8c93da64d4d387761c8d5d230f3576430a
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-02-26 12:15:15 -0300

    Cadastro de Produtos

--- App/services/api.service.js
+++ App/services/api.service.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'http://10.91.60.189:4000'
-  baseURL: 'http://192.168.1.106:4000'
+  baseURL: 'http://localhost:4000' // Preencher o localhost com o Ip
 });
